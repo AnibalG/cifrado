794d6ccc9c284ddfd1db3fea2c531e941f037e07
Anibal Gamboa <anibalgamboac@gmail.com>
2017-05-27 18:25:38 -0500

    cifrado ciclico y morse sin aceptar argumentos

--- cifrado.c
+++ cifrado.c
@@ -3,11 +3,12 @@
 #define TAMANO 1024
 
 void cifrado( char cod[], char mensaje[], int llave);
-
+void morse(char *cod[], char mensaje[]);
 int main(){
 
 	char mensaje[TAMANO] = {0};
 	char cod[TAMANO] = {0};
+	char *morsearray[TAMANO] = {0};
 	printf("Cifrado ciclico \n");
 	printf("Ingrese un mensaje a cifrar:  ");
 	fgets(mensaje, 100, stdin);
@@ -16,7 +17,15 @@ int main(){
 	scanf("%d", &llave);
 
 	cifrado(cod, mensaje, llave);
+	morse(morsearray, mensaje);
 	printf("Mensaje cifrado: %s", cod);
+	printf("Codigo Morse: ");
+	for(int i = 0; morsearray[i] != '\0'; i++)
+	{
+   		printf("%s", morsearray[i]);
+	}
+	printf("\n");
+
 	return(0);
 
 }
@@ -48,4 +57,124 @@ void cifrado(char cod[TAMANO], char mensaje[TAMANO], int llave){
 		cod[i] = caracter;
 
 	}
+}
+
+
+void morse(char *cod[TAMANO], char mensaje[TAMANO]){
+	for(int i = 0; mensaje[i] != '\0'; i++){
+		char car = mensaje[i];
+		switch(car){
+			case 'A':
+			case 'a':
+				cod[i] = " .- ";
+     				break;
+			case 'B':
+                        case 'b':
+                                cod[i] = " -...";
+                                break;
+			case 'C':
+                        case 'c':
+                                cod[i] = " -.-.";
+                                break;
+			case 'D':
+                        case 'd':
+                                cod[i] = " -..";
+                                break;
+			case 'E':
+                        case 'e':
+                                cod[i] = " .";
+                                break;
+			case 'F':
+                        case 'f':
+                                cod[i] = " ..-.";
+                                break;
+			case 'G':
+                        case 'g':
+                                cod[i] = " --.";
+                                break;
+			case 'H':
+                        case 'h':
+                                cod[i] = " ....";
+                                break;
+			case 'I':
+                        case 'i':
+                                cod[i] = " ..";
+                                break;
+                        case 'J':
+                        case 'j':
+                                cod[i] = " .---";
+                                break;
+                        case 'K':
+                        case 'k':
+                                cod[i] = " -.-";
+                                break;
+                        case 'L':
+                        case 'l':
+                                cod[i] = " .-..";
+                                break;
+                        case 'M':
+                        case 'm':
+                                cod[i] = " --";
+                                break;
+                        case 'N':
+                        case 'n':
+                                cod[i] = " -.";
+                                break;
+                        case 'O':
+                        case 'o':
+                                cod[i] = " ---";
+                                break;
+                        case 'P':
+                        case 'p':
+                                cod[i] = " .--.";
+                                break;
+			case 'Q':
+                        case 'q':
+                                cod[i] = " --.-";
+                                break;
+                        case 'R':
+                        case 'r':
+                                cod[i] = " .-.";
+                                break;
+                        case 'S':
+                        case 's':
+                                cod[i] = " ...";
+                                break;
+                        case 'T':
+                        case 't':
+                                cod[i] = " -";
+                                break;
+                        case 'U':
+                        case 'u':
+                                cod[i] = " ..-";
+                                break;
+                        case 'V':
+                        case 'v':
+                                cod[i] = " ...-";
+                                break;
+                        case 'W':
+                        case 'w':
+                                cod[i] = " .--";
+                                break;
+			case 'X':
+                        case 'x':
+                                cod[i] = " -..-";
+                                break;
+                        case 'Y':
+                        case 'y':
+                                cod[i] = " -.--";
+                                break;
+                        case 'Z':
+                        case 'z':
+                                cod[i] = " --..";
+                                break;
+			case ' ':
+                                cod[i] = " /";
+                                break;
+
+
+		}
+	}
+
+
 }
