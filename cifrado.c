f1503a032786c35a5039ebf98e0ad52b64d13746
Anibal Gamboa <anibalgamboac@gmail.com>
2017-05-27 18:57:57 -0500

    parte 1 y parte 2

--- cifrado.c
+++ cifrado.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
-
+#include <string.h>
+#include <stdlib.h> 
 #define TAMANO 1024
 
 void cifrado( char cod[], char mensaje[], int llave);
 void morse(char *cod[], char mensaje[]);
-int main(){
+int main(int argc, char **argv){
+	if(argc == 3){
+		printf("Cifrado Ciclico: ");
+		
+       		char cod[TAMANO] ={0};
 
+		int llave = atoi(argv[1]);
+		cifrado(cod, argv[2], llave);
+        	printf("Mensaje cifrado: %s", cod);
+		return (0);
+
+	}
 	char mensaje[TAMANO] = {0};
 	char cod[TAMANO] = {0};
 	char *morsearray[TAMANO] = {0};
