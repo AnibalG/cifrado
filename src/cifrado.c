14a061222d70788cc9719639dc771fbb9f287f5b
Anibal Gamboa <anibalgamboac@gmail.com>
2017-06-04 18:28:26 -0500

    cifrado.c actualizado

--- src/cifrado.c
+++ src/cifrado.c
@@ -4,6 +4,8 @@
 
 char* cifrado(char* mensaje, int llave){
 
+	char *cod = malloc(sizeof(mensaje));
+
 	for(int i = 0; mensaje[i] != '\0'; i++){
 		char caracter = mensaje[i];
 		if(caracter >= 'a' && caracter <= 'z' ){
@@ -22,19 +24,22 @@ char* cifrado(char* mensaje, int llave){
                         if (caracter > 'Z'){
                                 caracter = caracter - 'Z' + 'A' -1;
 			}
-		else if (caracter < 'A'){
+                        else if (caracter < 'A'){
                                 caracter = caracter + 'Z' - 'A' +1;
 			}
 		}
-		mensaje[i] = caracter;
+		cod[i] = caracter;
 
 	}
-	return mensaje;
+	return cod;
 }
 
 char* cifradoAutollave(char* mensaje, char* llave){
 
-	strcat(llave, mensaje);
+	char *cifrado = malloc(sizeof(mensaje));
+	strcpy(cifrado, mensaje);
+
+	strcat(llave, cifrado);
 
 	for(int i = 0; mensaje[i] != '\0'; i++){
                 char c1 = mensaje[i];
@@ -47,21 +52,27 @@ char* cifradoAutollave(char* mensaje, char* llave){
                 else if (caracter < 'A'){
                 	caracter = caracter + 'Z' - 'A' +1;
                 }
-		mensaje[i] = caracter;
+		cifrado[i] = caracter;
         }
-	return mensaje;
+	return cifrado;
 }
 
-char* cifradoContraseña(char* mensaje, char* llave){
-	strcat(llave, "ABCDEFGHIJKLMNOPQRSTUVWXY");
+char* cifradoContrasena(char* mensaje, char* llave){
+	char *cod = malloc(sizeof(mensaje));
+
+	strcat(llave, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
 	for(int i = 0; mensaje[i] != '\0'; i++){
 		char caracter = mensaje[i];
 		if(caracter >= 'A' && caracter <= 'Z'){
-			mensaje[i] = llave[caracter- 'A']
+			cod[i] = llave[caracter - 'A'];
+		}
+		else{
+			cod[i] = caracter;
 		}
 	}
 
-	return mensaje;
+
+	return cod;
 
 }
 
