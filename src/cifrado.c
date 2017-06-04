d2119b36f67a328290d60093a6cc62214cb6663d
SteevenRodriguez <rodriguez_steven11@hotmail.com>
2017-06-04 18:42:53 -0500

    funcion autollave completa

--- src/cifrado.c
+++ src/cifrado.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include "cifrado.h"
-
+#define TAMANO 10000
 char* cifrado(char* mensaje, int llave){
 
 	char *cod = malloc(sizeof(mensaje));
@@ -36,24 +36,73 @@ char* cifrado(char* mensaje, int llave){
 
 char* cifradoAutollave(char* mensaje, char* llave){
 
-	char *cifrado = malloc(sizeof(mensaje));
-	strcpy(cifrado, mensaje);
+	char* cifrado = (char *) malloc(sizeof(char) * TAMANO);
 
-	strcat(llave, cifrado);
+	
 
-	for(int i = 0; mensaje[i] != '\0'; i++){
-                char c1 = mensaje[i];
-		char c2 = llave[i];
-                char caracter = c1 - 'A' + c2;
+	char mensajeA[TAMANO] = {0};
+	char llaveA[TAMANO] = {0};
 
-		if (caracter > 'Z'){
-                	caracter = caracter - 'Z' + 'A' -1;
-               	}
-                else if (caracter < 'A'){
-                	caracter = caracter + 'Z' - 'A' +1;
+	strcat( mensajeA, mensaje);
+	strcat( llaveA, llave);
+	strcat( llaveA, mensajeA);
+
+
+	int j = 0;
+	int d = 0;
+	for(int i = 0; i<strlen(llaveA); i++)
+	{
+		d = llaveA[j];
+		while( d<65 || (d>90 && d<97) ||  d>122 )
+                {
+                        j++;
+			d = llaveA[j];
                 }
-		cifrado[i] = caracter;
-        }
+                llaveA[i] = llaveA[j];
+                j++;
+	}					
+
+	int c = 0;
+	char letra = 'a';
+	int mayus = 1;
+	int noLetra = 0;
+	int spacSimbNum = 0;
+
+	for(int i = 0; i<= strlen(mensajeA); i++)
+	{
+		noLetra = 0;
+		mayus = 1;
+		c = mensajeA[i];
+
+		if( c<=122 && c>=97 )
+		{
+			c -= 32;
+			mayus = 0;
+		}
+
+		if( c<=90 && c>=65 )
+		{
+			c += ( llaveA[i-spacSimbNum] - 65);
+
+			if( c>90 )
+				c = (c-90) + 64;
+			if(mayus == 0)
+				c += 32;
+		}
+		else
+		{
+			noLetra = 1;
+			spacSimbNum++;
+		}
+
+		if( noLetra == 1 )
+			cifrado[i] = mensajeA[i];
+		else
+		{
+			letra = c;
+			cifrado[i] = letra;
+		}
+	}
 	return cifrado;
 }
 
